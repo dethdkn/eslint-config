800b26615515ecefc76ddff2bb82e428484c4695
Gabriel Rosa <gabrielrosa@cbpf.br>
2024-06-01 01:12:56 -0300

    🐛 Removed promise/no-multiple-resolved

--- src/rules/promise.ts
+++ src/rules/promise.ts
@@ -7,7 +7,8 @@ export default {
   rules: {
     'promise/always-return': ['error'],
     'promise/catch-or-return': ['error'],
-    'promise/no-multiple-resolved': ['error'],
+    // https://github.com/eslint-community/eslint-plugin-promise/issues/472
+    // 'promise/no-multiple-resolved': ['error'],
     'promise/no-nesting': ['error'],
     'promise/no-new-statics': ['error'],
     'promise/no-promise-in-callback': ['error'],
